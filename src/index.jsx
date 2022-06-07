997044b5abbb1de57e7a85f01b274e304c359004
Hamed Ouattara <hamedkiri24@gmail.com>
2022-06-07 02:16:41 +0200

    version finale du projet

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+//import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
+import Profile from './pages/Profile'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Error from './components/Error'
 import GlobalStyle from './utils/style/GlobalStyle'
 import { ThemeProvider, SurveyProvider } from './utils/context'
-
+import { createRoot } from 'react-dom/client'
 const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
+const root = createRoot(container)
+
 root.render(
   <React.StrictMode>
     <Router>
@@ -24,10 +26,12 @@ root.render(
             <Route exact path="/" element={<Home />} />
 
             <Route path="/survey/:questionNumber" element={<Survey />} />
+
             <Route path="/results" element={<Results />} />
 
             <Route path="/freelances" element={<Freelances />} />
 
+            <Route path="/profile/:id" element={<Profile />} />
             <Route path="*" element={<Error />} />
           </Routes>
           <Footer />
@@ -36,3 +40,30 @@ root.render(
     </Router>
   </React.StrictMode>
 )
+
+/*ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProvider>
+        <SurveyProvider>
+          <GlobalStyle />
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+
+            <Route path="/survey/:questionNumber" element={<Survey />} />
+
+            <Route path="/results" element={<Results />} />
+
+            <Route path="/freelances" element={<Freelances />} />
+
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+          <Footer />
+        </SurveyProvider>
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)*/
